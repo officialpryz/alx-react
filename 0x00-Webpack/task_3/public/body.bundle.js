3596abc7c3c31092bcaf8ed6fe70d273317fd83c
officialpryz <afolabipraise43@gmail.com>
2024-08-19 19:04:11 +0100

    commit

---
+++ 0x00-Webpack/task_3/public/body.bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+(self["webpackChunktask_3"] = self["webpackChunktask_3"] || []).push([["body"],{},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__(null));
+/******/ }
+]);
